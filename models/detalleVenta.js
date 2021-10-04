801ab6a5095c5f1be426cd3ea9b2202192ef827e
mgcb <melanie.corletto@sansano.usm.cl>
2021-10-04 14:44:59 -0300

    actualización en modelo en mongo

--- models/detalleVenta.js
+++ models/detalleVenta.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 
 //Declaracion modelo detalleVenta
 const detalleVentaSchema = new mongoose.Schema({
-    cantidad: Int
+    cantidad: Int,
+    idVenta: {
+        type: Schema.Types.ObjectId,
+        ref: 'Venta'
+    },
+    idProducto: {
+        type: Schema.Types.ObjectId,
+        ref: 'Producto'
+    }
 });
 
 module.exports = mongoose.model('DetalleVenta', detalleVentaSchema);
